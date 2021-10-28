bc15a9a45ab6ec16ac45341acf297565c3631323
CapezOnMyBack <30703206+CapezOnMyBack@users.noreply.github.com>
2021-10-28 08:57:23 +0200

    Update GeneticMutator.py
    Finished mutator functions

--- GeneticMutator.py
+++ GeneticMutator.py
@@ -7,11 +7,11 @@ from BetterNN import Network
 
 margin = 2
 
-def mutator(w, b, margin) -> Network:
+def mutator_w(w, margin) -> Network:
+    
     mutated_weights = []
-    mutated_biases = []
     delta_w = []
-
+    
     for weights in w:
 
         for l in weights:
@@ -24,6 +24,29 @@ def mutator(w, b, margin) -> Network:
 
         mutated_weights.append(weights + np.array(delta_w).reshape(weights.shape))
         delta_w.clear()
+        
+        w = mutated_weights
+        return w
+        
+def mutator_b(b, margin) -> Network:
+    mutated_biases = []
+    delta_b = []
+     
+    for biases in b:
+        
+        for l in biases:
+            
+            for i in l:
+                
+                low = i * (1.0 - margin / 100)
+                high = i * (1.0 + margin / 100)
+                delta_b.append(random.uniform(low, high))
+                
+        mutated_biases.append(biases + np.array(delta_b).reshape(biases.shape))
+        delta_b.clear()
+        
+    b = mutated_biases
+    
+    return b
 
-    # TODO: MUTATOR FERTIG MACHEN GENAU GLEICH FÜR BIASES WIE BEI WEIGHTS
     # TODO: Ändern zu np.random.unify(), weil es arrays ebenfalls annimmt... ;-;
